612ac92e8f9fa1866011f5d47f6f81bf3e5e2a18
Dragon <1239209219@qq.com>
2022-10-27 21:27:03 +0800

    master 1.12

--- src/main/java/com/dragon/reggie/controller/EmployeeController.java
+++ src/main/java/com/dragon/reggie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.dragon.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dragon.reggie.common.R;
 import com.dragon.reggie.entity.Employee;
 import com.dragon.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
@@ -90,5 +89,30 @@ public class EmployeeController {
         employeeService.save(employee);
         return R.success("新增员工成功");
 
+    }
+
+    /**
+     * TODO 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        log.info("page = {},pageSize = {},name = {}",page,pageSize,name);
+
+        //TODO 1.构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+        //TODO 2.构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //TODO 3.添加过滤条件
+        queryWrapper.like(!StringUtils.isEmpty(name),Employee::getName,name);
+        //TODO 4.添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        //TODO 5 .执行查询
+        employeeService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
     }
 }
